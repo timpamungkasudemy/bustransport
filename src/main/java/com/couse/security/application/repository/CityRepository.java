1137349a9a89f9ef5a8d6ab700ad34cd90e38324
timpamungkas <timotius.pamungkas@gmail.com>
2025-06-05 22:47:28 +0700

    chore: add missing annotations

--- src/main/java/com/couse/security/application/repository/CityRepository.java
+++ src/main/java/com/couse/security/application/repository/CityRepository.java
@@ -3,9 +3,11 @@ package com.couse.security.application.repository;
 import java.util.UUID;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.couse.security.application.entity.City;
 
+@Repository
 public interface  CityRepository  extends PagingAndSortingRepository<City, UUID> {
 
 }
--- src/main/java/com/couse/security/application/repository/CountryRepository.java
+++ src/main/java/com/couse/security/application/repository/CountryRepository.java
@@ -5,8 +5,11 @@ import java.util.UUID;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
 import com.couse.security.application.entity.Country;
 
+@Repository
 public interface CountryRepository extends PagingAndSortingRepository<Country, UUID> {
     Optional<Country> findByCountryCodeAndDeletedFalse(String countryCode);
     Optional<Country> findByCountryNameAndDeletedFalse(String countryName);
