7b6b8d6b43c18827bf3e5826ef80c7bfde9baf4d
timpamungkas <timotius.pamungkas@gmail.com>
2025-06-05 22:48:32 +0700

    repo

--- src/main/java/com/couse/security/application/repository/CityRepository.java
+++ src/main/java/com/couse/security/application/repository/CityRepository.java
@@ -2,12 +2,12 @@ package com.couse.security.application.repository;
 
 import java.util.UUID;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.couse.security.application.entity.City;
 
 @Repository
-public interface  CityRepository  extends PagingAndSortingRepository<City, UUID> {
+public interface  CityRepository  extends JpaRepository<City, UUID> {
 
 }
--- src/main/java/com/couse/security/application/repository/CountryRepository.java
+++ src/main/java/com/couse/security/application/repository/CountryRepository.java
@@ -1,16 +1,16 @@
 package com.couse.security.application.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.couse.security.application.entity.Country;
 
 @Repository
-public interface CountryRepository extends PagingAndSortingRepository<Country, UUID> {
+public interface CountryRepository extends JpaRepository<Country, UUID> {
     Optional<Country> findByCountryCodeAndDeletedFalse(String countryCode);
     Optional<Country> findByCountryNameAndDeletedFalse(String countryName);
     List<Country> findAllByDeletedFalse();
